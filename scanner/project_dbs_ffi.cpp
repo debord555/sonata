8e9180ed8f20ba9291e2bdf4b0114e45e23dc994
Debasish Bordoloi <debasishbordoloi555@gmail.com>
2024-12-06 15:45:03 +0530

    removed debugging prints

--- scanner/project_dbs_ffi.cpp
+++ scanner/project_dbs_ffi.cpp
@@ -1,16 +1,17 @@
-#include <nlohmann/json.hpp>
-#include <sqlite3.h>
-#include <iostream>
-#include <list>
-#include <filesystem>
-
-#include "misc.hpp"
-#include "tag_functions.hpp"
-#include "database_functions.hpp"
 #include "project_dbs_ffi.hpp"
 
-int parseInputJSON(nlohmann::json json_input, std::vector<std::string> &search_paths, std::string &database_location, std::string &album_art_directory)
-{
+#include <sqlite3.h>
+
+#include <filesystem>
+#include <iostream>
+#include <list>
+#include <nlohmann/json.hpp>
+
+#include "database_functions.hpp"
+#include "misc.hpp"
+#include "tag_functions.hpp"
+
+int parseInputJSON(nlohmann::json json_input, std::vector<std::string> &search_paths, std::string &database_location, std::string &album_art_directory) {
     album_art_directory = json_input["album_art_directory"];
     database_location = json_input["database_location"];
     for (std::string path : json_input["search_paths"])
@@ -18,69 +19,51 @@ int parseInputJSON(nlohmann::json json_input, std::vector<std::string> &search_p
     return 0;
 }
 
-int update(const char *input)
-{
+int update(const char *input) {
     nlohmann::json json_input = nlohmann::json::parse(input);
     std::vector<std::string> search_paths;
     std::string database_location, album_art_directory;
     parseInputJSON(json_input, search_paths, database_location, album_art_directory);
 
-    
-
     sqlite3 *db;
-    if (sqlite3_open(database_location.c_str(), &db) != SQLITE_OK)
-    {
+    if (sqlite3_open(database_location.c_str(), &db) != SQLITE_OK) {
         sqlite3_close(db);
-        std::cout << "Error while opening database.\n";
+        log("Error while opening database.\n");
         return -1;
     }
 
     sqlite3_exec(db, "PRAGMA foreign_keys = 1;", NULL, NULL, NULL);
 
-    if (!std::filesystem::exists(std::filesystem::path(album_art_directory)) || !std::filesystem::is_directory(std::filesystem::path(album_art_directory)))
-    {
+    if (!std::filesystem::exists(std::filesystem::path(album_art_directory)) || !std::filesystem::is_directory(std::filesystem::path(album_art_directory))) {
         std::remove(album_art_directory.c_str());
         std::filesystem::create_directory(std::filesystem::path(album_art_directory));
     }
 
     std::unordered_set<std::filesystem::path> existing = getFileLocations(db);
 
-    std::cout << existing.size() << " files found in database already!" << std::endl;
-
     std::list<std::string> files;
 
-    for (std::string path : search_paths)
-    {
+    for (std::string path : search_paths) {
         for (auto &item : getFiles(path))
             files.push_back(item);
     }
 
-    for (std::list<std::string>::iterator it = files.begin(); it != files.end();)
-    {
+    for (std::list<std::string>::iterator it = files.begin(); it != files.end();) {
         std::filesystem::path p(*it);
-        if (existing.find(p) != existing.end())
-        {
-            std::cout << "Duplicate file: " << *it << std::endl;
+        if (existing.find(p) != existing.end()) {
             existing.erase(p);
             it = files.erase(it);
-        }
-        else
+        } else
             ++it;
     }
 
-    std::cout << "Scanning " << files.size() << " files." << std::endl;
-
-    for (std::string item : files)
-    {
-        if (endsWith(item, ".mp3") || endsWith(item, ".flac"))
-        {
+    for (std::string item : files) {
+        if (endsWith(item, ".mp3") || endsWith(item, ".flac")) {
             Metadata m;
             if (getMetadata(item, m) == 0)
                 addSong(db, m, album_art_directory);
         }
     }
-
-    std::cout << "Deleting " << existing.size() << " entries from DB " << std::endl;
 
     for (std::filesystem::path item : existing)
         deleteSongByLocation(db, item.generic_u8string());
@@ -88,7 +71,7 @@ int update(const char *input)
     deleteUselessAlbums(db);
     deleteUselessArtists(db);
     deleteUselessGenres(db);
-    //deleteUselessAlbumArt(db, album_art_directory);
+    // deleteUselessAlbumArt(db, album_art_directory);
 
     sqlite3_close(db);
     return 0;
